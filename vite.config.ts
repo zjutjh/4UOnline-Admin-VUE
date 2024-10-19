e544f3d4aaa8a07749d434598f9ade438da6f141
maojianhui <3605828735@qq.com>
2024-10-19 14:16:40 +0800

    chore: 添加env文件，配置proxy代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
+
+const env = loadEnv("development", process.cwd(), "");
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +13,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: env.HOST,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
     }
   }
 });
