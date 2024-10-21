dee3fcf92c55d486d8fa8bb67d1187bc65907e16
maojianhui <3605828735@qq.com>
2024-10-21 10:29:28 +0800

    chore: 安装Pinia持久化插件, 卸载axios库

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import "./index.css";
 import { createApp } from "vue";
-import { createPinia } from "pinia";
+import pinia from "./stores/createPinia";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
