700c779f3e6841237e03857a2e7e96577d479301
樱岛麻衣 <maojianhui2006@gmail.com>
2024-12-02 17:23:41 +0800

    feat: 初步添加首页页面 (#6)

--- src/pages/HomePage/index.vue
+++ src/pages/HomePage/index.vue
@@ -1,6 +1,32 @@
 <template>
-    <div>首页</div>
+  <div class="relative">
+    <div class="font-bold text-5xl absolute left-24 top-12">
+      欢迎你，来自{{ department }}的{{ loginStore.userId }}
+    </div>
+    <div class="font-bold text-4xl absolute left-64 top-64">
+      今日新增反馈{{ newFeedback }}条，点击前往处理
+      <el-button
+        size="large"
+        type="primary"
+        class="ml-5 transform -translate-y-1"
+        @click="onToFeedback"
+      >
+        前往处理
+      </el-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { useLoginStore } from "@/stores";
+import router from "@/router";
+const department = ref("{{department}}");
+const loginStore = useLoginStore();
+
+const newFeedback = ref<number>(0);
+
+const onToFeedback = () => {
+  router.push("/main/question-feedback");
+};
 </script>
